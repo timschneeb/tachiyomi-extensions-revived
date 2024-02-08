c71a2280f7db6fcfcae32c0c5272f4066a8aa8de
Sandeep Karanam <sandeepjak2007@gmail.com>
2024-02-08 22:03:03 +0530

    Issue #39
    Issue #39 Assured

--- multisrc/overrides/madara/asurascansus/src/AsuraScansUs.kt
+++ multisrc/overrides/madara/asurascansus/src/AsuraScansUs.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.revived.en.asurascansus
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class AsuraScansUs : Madara("Asura Scans.us (unoriginal)", "https://asurascans.us", "en") {
+class AsuraScansUs : Madara("Asura Scans.us (unoriginal)", "https://asurascans.com.tr", "en") {
     override val useNewChapterEndpoint = true
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
